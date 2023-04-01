4cfcd97ece81dc10bc23387e12c807a2c3a9d938
StellaCea <stela.ceaicovscaia@hotmail.com>
2023-04-01 22:38:22 +0200

    Updated PORT

--- index.js
+++ index.js
@@ -271,7 +271,7 @@ app.use((err, req, res, next)=>{
 
 
 //listen for requests
-const port = process.env.PORT || 8080;
-app.listen(port, "0.0.0.0",()=>{
-    console.log("Listening on Port " + port);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, "0.0.0.0",()=>{
+    console.log("Listening on Port " + PORT);
 })
