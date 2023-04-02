be7ab2cc4ee9fc7af9fabe59048bf66752e506c6
StellaCea <stela.ceaicovscaia@hotmail.com>
2023-04-02 11:09:56 +0200

    updated MongoParse

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 //mongoose.connect('mongodb://localhost:27017/cfDB', {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.CONNECTION_URI).catch((error) => handleError(error));
 
 const express = require ('express'),
     morgan = require ('morgan'),
