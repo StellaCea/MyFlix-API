b4e87d5d7ad8cff88fabd5c64966b4dbb0fd16fc
StellaCea <stela.ceaicovscaia@hotmail.com>
2023-04-02 10:17:30 +0200

    added a procfile

--- index.js
+++ index.js
@@ -268,7 +268,7 @@ app.use((err, req, res, next)=>{
 
 
 //listen for requests
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port, "0.0.0.0",()=>{
     console.log("Listening on Port " + port);
 })
