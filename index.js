a48957316c4f4dce7dd20b46ba2df7cf2684f09e
StellaCea <stela.ceaicovscaia@hotmail.com>
2023-05-13 11:47:45 +0200

    updated index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const cors = require("cors");
 //app.use(cors()); //sets the app to allow requests from all origins
 
 //Only certain origins will be given access
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://my-flix-api.netlify.app'];
 
 app.use(cors({
     origin: (origin, callback) => {
